eff9e152e2abdb7eed0ad09ddf208594f432bc6c
Ushapramod <ushanischay2016@gmail.com>
2025-02-07 12:35:03 +0530

    CountTheOccuranceOfCharacters

--- src/JavaProgramms/CountTheOccuranceOfCharacters.java
+++ src/JavaProgramms/CountTheOccuranceOfCharacters.java
@@ -1,6 +1,21 @@
 
 package JavaProgramms;
 
-public class CountTheOccuranceOfCharacters {
+import java.util.HashMap;
 
+public class CountTheOccuranceOfCharacters {
+	public static void main(String[] args) {
+		
+	
+	String name = "ushapramod";
+
+	char[] arr = name.toCharArray();
+
+	HashMap<Character, Integer> map = new HashMap<>();
+	for (int i = 0; i < arr.length; i++) {
+		char c = arr[i];
+		map.put(c, map.getOrDefault(c, 0)+1);
+	}
+System.out.println(map);
+}
 }
